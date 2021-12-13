4f7fc2c92f02910d394efd1118ac60224e1742c7
Santino Speranza <speranzasantino60@gmail.com>
2021-12-13 11:40:16 -0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Rutas from './Rutas/Rutas'
-//import CartProvider from './components/context/useContext'
+import CartProvider from './components/context/useContext'
 const App = () => {
   return (
     <div className='App'>
+      <CartProvider>
       <Rutas />
+      </CartProvider>
 
     </div>
   )
